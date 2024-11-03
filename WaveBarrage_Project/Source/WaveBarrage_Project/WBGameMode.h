fad8478e253330376ece38c57b4bff6522951ab8
museongchoi <antjd3303@hs.ac.kr>
2024-11-03 21:01:17 +0900

    CMS : Feat WPGameMode C++

---
+++ WaveBarrage_Project/Source/WaveBarrage_Project/WBGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WBGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAVEBARRAGE_PROJECT_API AWBGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
