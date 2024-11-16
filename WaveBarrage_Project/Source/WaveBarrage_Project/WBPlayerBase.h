91056b5d3c40e1d4baf0dbd9c27cba5fffaaa864
navysys <51705795+navysys@users.noreply.github.com>
2024-11-17 02:01:26 +0900

    20241117-test2

---
+++ WaveBarrage_Project/Source/WaveBarrage_Project/WBPlayerBase.h
@@ -0,0 +1,142 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "WBPlayerBase.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class USpringArmComponent;
+class UCameraComponent;
+class UBoxComponent;
+class AWBMonsterBase;
+class AActor;
+class AWBWeaponBase;
+
+UCLASS()
+class WAVEBARRAGE_PROJECT_API AWBPlayerBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AWBPlayerBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void ConfigureInputMapping();
+
+	// 공격 사용 함수
+	UFUNCTION()
+	void AutomaticAiming();
+
+	UFUNCTION()
+	void CheckAttack();
+
+	UFUNCTION()
+	void AttackFire();
+
+	UFUNCTION()
+	void CursorHitAiming();
+
+	UFUNCTION()
+	void DefaultAttackSettings();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UCameraComponent> Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USceneComponent> Box1;
+
+	UPROPERTY()
+	TObjectPtr<USceneComponent> Box2;
+
+	UPROPERTY()
+	TObjectPtr<USceneComponent> Box3;
+
+	UPROPERTY()
+	TObjectPtr<USceneComponent> Box4;
+
+	UPROPERTY()
+	TObjectPtr<USceneComponent> Box5;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> MonsterSpawnPosition1;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> MonsterSpawnPosition2;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> MonsterSpawnPosition3;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> MonsterSpawnPosition4;
+
+	UPROPERTY(VisibleAnywhere)
+	TArray<USceneComponent*> MonsterSpawnPositions;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> InputMapping;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> IA_Move;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> IA_Skill_E;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> IA_Skill_R;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> IA_AutoMode;
+
+	void Move(const FInputActionValue& Value);
+
+	void SkillE();
+
+	void SkillR();
+
+	void ToggleAutoMode();
+
+	UPROPERTY()
+	TObjectPtr<APlayerController> MyPlayerController;
+
+	// 캐릭터 각 전용 무기
+	UPROPERTY(EditAnywhere, Category = "Weapon")
+	TSubclassOf<AWBWeaponBase> ChampionOnlyWeapon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	bool bAutoMode;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float ClosestDistance;
+
+	UPROPERTY()
+	TObjectPtr<AActor> ClosestEnemy;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	bool bIsAttacking;
+
+	UPROPERTY()
+	AWBWeaponBase* SpawnedWeapon;
+
+	FTimerHandle FTimerHandle_CheckAttack;
+	FTimerHandle FTimerHandle_AutomaticAiming;
+	FTimerHandle FTimerHandle_CursorAiming;
+};
