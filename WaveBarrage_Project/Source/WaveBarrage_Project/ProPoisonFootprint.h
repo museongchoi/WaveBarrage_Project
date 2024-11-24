4208f23b4abb746b50e069e0eb3a057db52874bd
GyoNyeong <rysud123@naver.com>
2024-11-24 20:56:11 +0900

    HP UI connect

--- WaveBarrage_Project/Source/WaveBarrage_Project/ProPoisonFootprint.h
+++ WaveBarrage_Project/Source/WaveBarrage_Project/ProPoisonFootprint.h
@@ -26,7 +26,7 @@ protected:
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	void OnSphereOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 
-	
+
 
 public:
 
