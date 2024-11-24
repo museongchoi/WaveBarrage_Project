61ab4b15d98fc936b7191d9655d0a1fc7775c930
GyoNyeong <rysud123@naver.com>
2024-11-25 00:49:14 +0900

    WhirlWind 발사체 개수 적용

--- WaveBarrage_Project/Source/WaveBarrage_Project/WeaponWhirlwind.cpp
+++ WaveBarrage_Project/Source/WaveBarrage_Project/WeaponWhirlwind.cpp
@@ -12,6 +12,8 @@ AWeaponWhirlwind::AWeaponWhirlwind()
 	CoolDown = 7.0f;
 	Damage = 55;
 	Duration = 7;
+	ProjectileCount = 2;
+
 
 	SetReplicates(true);
 	SetReplicateMovement(true);
@@ -27,68 +29,26 @@ void AWeaponWhirlwind::C2S_SpawnWhirlwindBlade_Implementation()
 {
 	if (!HasAuthority()) // 서버에서만 스폰 실행
 	return;
-	
-
+	CalculateAttackStatus();
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.Owner = this;
 	SpawnParams.Instigator = GetInstigator();
 
 	if (ProjectileClass && OwnerCharacter && ProjectileSpawnPoint)
 	{
-		switch (WeaponLevel)
+		for (int i = 1; i <= ProjectileCount; i++)
 		{
-		case 1:
-		case 2:
-			for (int i = 1; i <= 2; i++)
+			AProWhirlwindBlade* WhirlwindBlade = GetWorld()->SpawnActor<AProWhirlwindBlade>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator, SpawnParams);
 			{
-				AProWhirlwindBlade* WhirlwindBlade = GetWorld()->SpawnActor<AProWhirlwindBlade>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator, SpawnParams);
-				{
-					WhirlwindBlade->SpawnLocationByRadians = 180 * i;
+				WhirlwindBlade->SpawnLocationByRadians = 360 / ProjectileCount * i ;
 
-					int32 FinalDamage = CalculateFinalDamage();
-					WhirlwindBlade->SetDamage(FinalDamage);
-					WhirlwindBlade->CanCollision = true;
-					WhirlwindBlade->SetReplicates(true); // 복제 설정
-					WhirlwindBlade->SetOwner(this);
-					WhirlwindBlade->SetLifeSpan(Duration);
-				}
+				int32 FinalDamage = CalculateFinalDamage();
+				WhirlwindBlade->SetDamage(FinalDamage);
+				WhirlwindBlade->CanCollision = true;
+				WhirlwindBlade->SetReplicates(true); // 복제 설정
+				WhirlwindBlade->SetOwner(this);
+				WhirlwindBlade->SetLifeSpan(Duration);
 			}
-
-			break;
-		case 3:
-		case 4:
-			for (int i = 1; i <= 4; i++)
-			{
-				AProWhirlwindBlade* WhirlwindBlade = GetWorld()->SpawnActor<AProWhirlwindBlade>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator, SpawnParams);
-				{
-					WhirlwindBlade->SpawnLocationByRadians = 90 * i;
-
-					int32 FinalDamage = CalculateFinalDamage();
-					WhirlwindBlade->SetDamage(FinalDamage);
-					WhirlwindBlade->CanCollision = true;
-					WhirlwindBlade->SetReplicates(true); // 복제 설정
-					WhirlwindBlade->SetOwner(this);
-					WhirlwindBlade->SetLifeSpan(Duration);
-				}
-			}
-			break;
-		case 5:
-			for (int i = 1; i <= 6; i++)
-			{
-				AProWhirlwindBlade* WhirlwindBlade = GetWorld()->SpawnActor<AProWhirlwindBlade>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator, SpawnParams);
-				{
-					WhirlwindBlade->SpawnLocationByRadians = 60 * i;
-
-					int32 FinalDamage = CalculateFinalDamage();
-					WhirlwindBlade->SetDamage(FinalDamage);
-					WhirlwindBlade->CanCollision = true;
-					WhirlwindBlade->SetReplicates(true); // 복제 설정
-					WhirlwindBlade->SetOwner(this);
-					WhirlwindBlade->SetLifeSpan(Duration);
-				}
-
-			}
-			break;
 		}
 	}
 
@@ -100,23 +60,23 @@ void AWeaponWhirlwind::BeginPlay()
 
 	if (HasAuthority())
 	{
+		CalculateAttackStatus();
 		GetWorld()->GetTimerManager().SetTimer(FTimerHandle_Fire, this, &AWeaponWhirlwind::Fire, 15.0f, true, 0.0f);
-		UE_LOG(LogTemp, Warning, TEXT("Spawned on %s"), HasAuthority() ? TEXT("Server") : TEXT("Client"));
+		
 	}
 
-	CalculateAttackStatus();
+	
 
 }
 
 void AWeaponWhirlwind::Fire()
 {
 	Super::Fire();
-	UE_LOG(LogTemp, Warning, TEXT("WhirlwindFire"));
-	
 
 	if (HasAuthority()) // 서버라면 직접 실행
 	{
-		UE_LOG(LogTemp, Warning, TEXT("WhirlwindFire On Server"));
+		CalculateAttackStatus();
+		UE_LOG(LogTemp, Warning, TEXT("Wind Projectile : %d "),MaxProjectileCnt);
 		/*FActorSpawnParameters SpawnPara;*/
 		if (!HasAuthority()) // 서버가 아닐 경우 실행하지 않음
 		{
@@ -129,64 +89,26 @@ void AWeaponWhirlwind::Fire()
 
 		if (ProjectileClass && OwnerCharacter && ProjectileSpawnPoint)
 		{
-
-
-			UE_LOG(LogTemp, Warning, TEXT("WhirlwindFire"));
-			switch (WeaponLevel)
+			for (int i = 1; i <= MaxProjectileCnt; i++)
 			{
-			case 1:
-			case 2:
-				for (int i = 1; i <= 2; i++)
+				AProWhirlwindBlade* WhirlwindBlade = GetWorld()->SpawnActor<AProWhirlwindBlade>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator, SpawnParams);
 				{
-					AProWhirlwindBlade* WhirlwindBlade = GetWorld()->SpawnActor<AProWhirlwindBlade>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator, SpawnParams);
-					{
-						WhirlwindBlade->SpawnLocationByRadians = 180 * i;
+					WhirlwindBlade->SpawnLocationByRadians = 360 / MaxProjectileCnt * i;
 
-						int32 FinalDamage = CalculateFinalDamage();
-						WhirlwindBlade->SetDamage(FinalDamage);
-						WhirlwindBlade->CanCollision = true;
-						WhirlwindBlade->SetLifeSpan(Duration);
-						UE_LOG(LogTemp, Warning, TEXT("Duration : %d"),Duration);
-					}
+					int32 FinalDamage = CalculateFinalDamage();
+					WhirlwindBlade->SetDamage(FinalDamage);
+					WhirlwindBlade->CanCollision = true;
+					WhirlwindBlade->SetReplicates(true); // 복제 설정
+					WhirlwindBlade->SetOwner(this);
+					WhirlwindBlade->SetLifeSpan(Duration);
 				}
-
-				break;
-			case 3:
-			case 4:
-				for (int i = 1; i <= 4; i++)
-				{
-					AProWhirlwindBlade* WhirlwindBlade = GetWorld()->SpawnActor<AProWhirlwindBlade>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator, SpawnParams);
-					{
-						WhirlwindBlade->SpawnLocationByRadians = 90 * i;
-
-						int32 FinalDamage = CalculateFinalDamage();
-						WhirlwindBlade->SetDamage(FinalDamage);
-						WhirlwindBlade->CanCollision = true;
-						WhirlwindBlade->SetLifeSpan(Duration);
-					}
-				}
-				break;
-			case 5:
-				for (int i = 1; i <= 6; i++)
-				{
-					AProWhirlwindBlade* WhirlwindBlade = GetWorld()->SpawnActor<AProWhirlwindBlade>(ProjectileClass, GetActorLocation(), FRotator::ZeroRotator, SpawnParams);
-					{
-						WhirlwindBlade->SpawnLocationByRadians = 60 * i;
-
-						int32 FinalDamage = CalculateFinalDamage();
-						WhirlwindBlade->SetDamage(FinalDamage);
-						WhirlwindBlade->CanCollision = true;
-						WhirlwindBlade->SetLifeSpan(Duration);
-					}
-
-				}
-				break;
 			}
 		}
 		
 	}
 	else // 클라이언트라면 서버에 요청
 	{
+
 		C2S_SpawnWhirlwindBlade();
 		
 	}
